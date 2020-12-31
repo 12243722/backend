eb1791857613fcbea112cbfe8693cc488788598d
liurushen <704790860@qq.com>
2020-12-31 08:59:52 +0800

    后端

--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,137 @@ router.get("/indexdetail",function(req,res,next){
       res.json(obj);
   })
 });
+router.get("/House",function(req,res,next){
+  let sqlSearch = `select * from sy_search_list`;
+  conn.query(sqlSearch, (err, results) => {
+      let obj = {
+        status:1
+      }
+      if (err) {
+        obj.msg = "失败"
+        throw err
+      }else{
+        obj.status=0;
+        obj.result = results;
+      }
+      res.json(obj);
+  })
+});
+router.get("/browse",function(req,res,next){
+  let sqlSearch = `select * from history`;
+  conn.query(sqlSearch, (err, results) => {
+      let obj = {
+        status:1
+      }
+      if (err) {
+        obj.msg = "失败"
+        throw err
+      }else{
+        obj.status=0;
+        obj.result = results;
+      }
+      res.json(obj);
+  })
+});
+router.get("/Landlord",function(req,res,next){
+  let sqlSearch = `select * from loader`;
+  conn.query(sqlSearch, (err, results) => {
+      let obj = {
+        status:1
+      }
+      if (err) {
+        obj.msg = "失败"
+        throw err
+      }else{
+        obj.status=0;
+        obj.result = results;
+      }
+      res.json(obj);
+  })
+});
+router.get("/Collect",function(req,res,next){
+  let sqlSearch = `select * from selectlist`;
+  conn.query(sqlSearch, (err, results) => {
+      let obj = {
+        status:1
+      }
+      if (err) {
+        obj.msg = "失败"
+        throw err
+      }else{
+        obj.status=0;
+        obj.result = results;
+      }
+      res.json(obj);
+      console.log(res);
+  })
+});
+router.get("/Hotlist",function(req,res,next){
+  let sqlSearch = `select * from collectlist`;
+  conn.query(sqlSearch, (err, results) => {
+      let obj = {
+        status:1
+      }
+      if (err) {
+        obj.msg = "失败"
+        throw err
+      }else{
+        obj.status=0;
+        obj.result = results;
+      }
+      res.json(obj);
+      console.log(res);
+  })
+});
+router.get("/homestay",function(req,res,next){
+  let sqlSearch = `select * from homestaylist`;
+  conn.query(sqlSearch, (err, results) => {
+      let obj = {
+        status:1
+      }
+      if (err) {
+        obj.msg = "失败"
+        throw err
+      }else{
+        obj.status=0;
+        obj.result = results;
+      }
+      res.json(obj);
+      console.log(res);
+  })
+});
+router.get("/parentsroom",function(req,res,next){
+  let sqlSearch = `select * from parentsroomlist`;
+  conn.query(sqlSearch, (err, results) => {
+      let obj = {
+        status:1
+      }
+      if (err) {
+        obj.msg = "失败"
+        throw err
+      }else{
+        obj.status=0;
+        obj.result = results;
+      }
+      res.json(obj);
+      console.log(res);
+  })
+});
+router.get("/goout",function(req,res,next){
+  let sqlSearch = `select * from gooutlist`;
+  conn.query(sqlSearch, (err, results) => {
+      let obj = {
+        status:1
+      }
+      if (err) {
+        obj.msg = "失败"
+        throw err
+      }else{
+        obj.status=0;
+        obj.result = results;
+      }
+      res.json(obj);
+      console.log(res);
+  })
+});
 module.exports = router;
